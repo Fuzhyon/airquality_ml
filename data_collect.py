4ba0ef8add240d950a7d026971f188fa4d7bb612
unknown <domingueschris34@gmail.com>
2020-04-16 16:25:28 +0200

    comment for datetime to timestamp function

--- data_collect.py
+++ data_collect.py
@@ -91,3 +91,11 @@ except KeyboardInterrupt:
 	df_airquality["Airquality"] = pan.Series(sensor_airquality, dtype=object)
 	df_airquality["Time"] = pan.Series(sensor_time)
 	print(df_airquality)
+
+# from datetime import datetime
+
+# # current date and time
+# now = datetime.now()
+
+# timestamp = datetime.timestamp(now)
+# print("timestamp =", timestamp)
